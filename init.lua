a86c66bce59493651b3ca09cf038ed9ab0952601
natewong1313 <natewong1@gmail.com>
2025-01-29 10:14:50 -0500

    move more plugins

--- init.lua
+++ init.lua
@@ -87,20 +87,7 @@ P.S. You can delete this when you're done too. It's your config now! :)
 require 'globals'
 require 'options'
 require 'keymaps'
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
+require 'autocmds'
 require 'lazy-install'
 require 'lazy-setup'
 -- The line beneath this is called `modeline`. See `:help modeline`
