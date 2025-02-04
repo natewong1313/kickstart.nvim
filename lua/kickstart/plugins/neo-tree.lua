8df332713ba995247fafe572cdb9450fec26a3d7
natewong1313 <natewong1@gmail.com>
2025-02-04 12:06:25 -0500

    remove unused plugin and float tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -13,16 +13,28 @@ return {
   keys = {
     { '\\', ':Neotree reveal<CR>:set number<CR>', desc = 'NeoTree reveal', silent = true },
   },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
+  config = function()
+    require('neo-tree').setup {
+      event_handlers = {
+
+        {
+          event = 'file_open_requested',
+          handler = function()
+            require('neo-tree.command').execute { action = 'close' }
+          end,
         },
       },
-      filtered_items = {
-        visible = true,
+      filesystem = {
+        window = {
+          mappings = {
+            ['\\'] = 'close_window',
+          },
+          position = 'float',
+        },
+        filtered_items = {
+          visible = true,
+        },
       },
-    },
-  },
+    }
+  end,
 }
