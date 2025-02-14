fc0438868d4e6ee66f98a92a95e17c27eb4c1bdf
natewong1313 <natewong1@gmail.com>
2025-02-14 11:17:43 -0500

    QOL keybinds

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -35,3 +35,17 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 vim.keymap.set('n', '1', '^', { noremap = true, silent = true })
 -- Ctrl s to save
 vim.keymap.set({ 'n', 'i', 'v' }, '<C-s>', '<cmd> w <cr>')
+
+-- <leader> e to show full error message in float
+vim.keymap.set('n', '<leader>e', function()
+  vim.diagnostic.open_float { focusable = true }
+  vim.diagnostic.open_float { focusable = true }
+end, { desc = 'Expand an Error into a float' })
+-- <Esc> to hide float
+vim.keymap.set('n', '<Esc>', function()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    if vim.api.nvim_win_get_config(win).relative == 'win' then
+      vim.api.nvim_win_close(win, false)
+    end
+  end
+end, { desc = 'Close opened float window' })
