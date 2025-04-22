0c6ce334ebfb163e1fb07d7273c1e4cbbdc4d6de
natewong1313 <natewong1@gmail.com>
2025-04-21 21:23:44 -0400

    Add typescript-tools

---
+++ lua/custom/plugins/typescript-tools.lua
@@ -0,0 +1,15 @@
+return {
+  'pmizio/typescript-tools.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  opts = {},
+  config = function()
+    require('typescript-tools').setup {
+      settings = {
+        jsx_close_tag = {
+          enable = true,
+          filetypes = { 'javascriptreact', 'typescriptreact' },
+        },
+      },
+    }
+  end,
+}
